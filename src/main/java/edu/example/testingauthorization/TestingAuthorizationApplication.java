4484d6854ae20a66e88eb6c55b48d18c65be4732
Gmakk <71002826+Gmakk@users.noreply.github.com>
2024-08-18 22:16:55 +0300

    configuration changes

--- src/main/java/edu/example/testingauthorization/TestingAuthorizationApplication.java
+++ src/main/java/edu/example/testingauthorization/TestingAuthorizationApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TestingAuthorizationApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TestingAuthorizationApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TestingAuthorizationApplication.class, args);
+    }
 
 }
